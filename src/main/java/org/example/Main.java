76ab38c806bb3d0f9690d3039db5c6465467d5d5
zrdzn <zrdzn9@gmail.com>
2023-10-04 08:23:54 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ChromeDriver Driver= new ChromeDriver();
+
+
+
+
+    }
+}
