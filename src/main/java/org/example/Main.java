d58a9df12b080fa25fb50fc89e15e5e3ba497322
zrdzn <zrdzn9@gmail.com>
2023-10-11 08:31:32 +0200

    Add empty lines

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,10 @@ package org.example;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         ChromeDriver webDriver = new ChromeDriver();
     }
+
 }
