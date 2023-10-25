66adf12e3bd263032c1edc389a4578a7884b35ac
zrdzn <zrdzn9@gmail.com>
2023-10-25 07:50:03 +0200

    Add loop

---
+++ src/main/java/org/example/loop/Loop.java
@@ -0,0 +1,17 @@
+package org.example.loop;
+
+public class Loop {
+
+    static String[] tablica;
+
+    public static void main(String[] args) {
+        tablica = new String[3];
+        String str1 = "Ala";
+        String str2 = "str2";
+        String str3 = "str3";
+        tablica[0] = str1;
+        tablica[1] = str2;
+        tablica[2] = str3;
+    }
+
+}
