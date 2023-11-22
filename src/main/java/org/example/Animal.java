7a9416425d3b93c82bad4d0947a1b75d59ce4ddf
zrdzn <zrdzn9@gmail.com>
2023-11-22 07:58:38 +0100

    Update animal

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Animal {
 
+    private final String specie;
 
+    public Animal(String specie) {
+        this.specie = specie;
+    }
+
+    public void speak() {
+        System.out.println("My nmame is " + specie);
+    }
+
+    public void feed(String food) {
+        System.out.println(food);
+    }
+
+    public void feed(String food, int amount) {
+        System.out.println(food + amount);
+    }
 
 }
